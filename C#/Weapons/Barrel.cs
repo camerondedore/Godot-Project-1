7e58838a38c3cd3fe8254221fcc76e7181970e81
camerondedore <camerondedore@gmail.com>
2022-07-24 16:22:09 -0700

    adding barrel spread

--- C#/Weapons/Barrel.cs
+++ C#/Weapons/Barrel.cs
@@ -6,12 +6,14 @@ public class Barrel : Spatial
     
     [Export]
     PackedScene bulletScene;
+    [Export]
+    float spread = 1;
 
 
 
     public override void _Ready()
     {
-
+        //GD.Randomize();
     }
 
 
@@ -21,10 +23,12 @@ public class Barrel : Spatial
         // load bullet resource
         var bullet = bulletScene.Instance() as Bullet;
 
-        // GlobalTransform only works when parents are Spatial
+        // get spread
+        var spreadForShot = new Vector3(GD.Randf() - 0.5f, GD.Randf() - 0.5f, GD.Randf() - 0.5f) * spread;
 
+        // GlobalTransform only works when parents are Spatial
         // set bullet position
-        bullet.LookAtFromPosition(GlobalTransform.origin, GlobalTransform.origin + GlobalTransform.basis.z * -1, Vector3.Up);
+        bullet.LookAtFromPosition(GlobalTransform.origin, GlobalTransform.origin + GlobalTransform.basis.z * -100 + spreadForShot, Vector3.Up);
         
         // set parent as root scene
         GetTree().Root.AddChild(bullet);
