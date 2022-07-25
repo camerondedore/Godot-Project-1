82f0160e401bad4e2db2ae1d656ccf659ac8df7a
camerondedore <camerondedore@gmail.com>
2022-07-24 20:16:07 -0700

    adding weapon audio

--- C#/Weapons/Weapon.cs
+++ C#/Weapons/Weapon.cs
@@ -5,11 +5,15 @@ public class Weapon : Spatial
 {
     
     [Export]
-    NodePath barrelPath;
+    NodePath barrelPath,
+        weaponAudioPath;
     [Export]
     float rateOfFire = 600;
+    [Export]
+    AudioStream fireSound;
     protected TimedDisconnector autoSear = new TimedDisconnector();
     Barrel barrel;
+    AudioStreamPlayer3D weaponAudio;
 
 
 
@@ -23,16 +27,32 @@ public class Weapon : Spatial
 
         // get barrel
         barrel = GetNode(barrelPath) as Barrel;
+
+        // get weapon audio
+        weaponAudio = GetNode(weaponAudioPath) as AudioStreamPlayer3D;
     }
 
 
 
     public override void _Process(float delta)
     {
+        GD.Print(GetChildCount());
+
         if(autoSear.Trip(PlayerInput.fire1))
         {
             // fire
             barrel.Fire();
+
+            // audio
+            weaponAudio.Stream = fireSound;
+            weaponAudio.Play();
+            // var newWeaponAudio = new AudioStreamPlayer3D();
+            // AddChild(newWeaponAudio);
+            // newWeaponAudio.Stream = fireSound;
+            // newWeaponAudio.MaxDistance = weaponAudio.MaxDistance;
+            // newWeaponAudio.MaxDb = weaponAudio.MaxDb;
+            // newWeaponAudio.Connect("finished", newWeaponAudio, "QueueFree");
+            // newWeaponAudio.Play();
         }
     }
 }
