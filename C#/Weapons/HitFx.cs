de0b86433b9c78ba44728a8f56eaa66f0de5c1a2
camerondedore <camerondedore@gmail.com>
2022-09-13 20:47:03 -0700

    working on blast and hit fx

--- C#/Weapons/HitFx.cs
+++ C#/Weapons/HitFx.cs
@@ -5,17 +5,76 @@ public class HitFx : Spatial
 {
 	
 	[Export]
-	NodePath mainFxPath;
+	NodePath mainFxPath,
+		blastFxPath;
+	[Export]
+	float life = 0.5f,
+		blastScaleRange = 0.4f,		
+		blastSpread = 0.1f,
+		blastLife = 0.1f,
+		blastSpeed = 5;
 	CPUParticles mainFx;
+	Spatial blastFx;
+	float startTime,
+		blastSpeedVariation;
 
 
 
 	public override void _Ready()
 	{
+		startTime = OS.GetTicksMsec() * 0.001f;
+
 		// get nodes
 		mainFx = GetNode<CPUParticles>(mainFxPath);
+		blastFx = GetNode<Spatial>(blastFxPath);
 
-		// play fx
+		// play particle fx
 		mainFx.Emitting = true;
+
+
+		// set up blast fx scale variation
+		blastSpeedVariation = (GD.Randf() - 0.5f) * 2 * blastScaleRange;
+
+		// set blast fx scale to zero
+		blastFx.Scale = Vector3.Zero;
+
+		// get new scale
+		//var scaleForBlast = 1 - (GD.Randf() - 0.5f) * 2 * blastScaleRange;
+		
+		// set new scale
+		// spatial had the Scale property for getting and setting
+		//blastFx.Scale = (blastFx.Scale * scaleForBlast);
+
+		// get new forward direction
+		var forwardForBlast = new Vector3(GD.Randf() - 0.5f, GD.Randf() - 0.5f, GD.Randf() - 0.5f) * blastSpread;
+		
+		// get new up direction
+		var upForBlast = new Vector3(GD.Randf() - 0.5f, GD.Randf() - 0.5f, GD.Randf() - 0.5f);
+
+		// set position and new forward direction
+		blastFx.LookAtFromPosition(GlobalTransform.origin, GlobalTransform.origin + GlobalTransform.basis.z * -1 + forwardForBlast, upForBlast);
 	}
+
+
+
+	public override void _Process(float delta)
+	{
+		var currentTime = OS.GetTicksMsec() * 0.001f;
+		// check life time
+		if(startTime + life < currentTime)
+		{
+			// destroy
+			QueueFree();
+		}
+
+		// check blast fx life time
+		if(startTime + blastLife < currentTime)
+		{
+			// hide
+			blastFx.Hide();
+		}
+
+		// scale blast fx
+		blastFx.Scale = (blastFx.Scale + Vector3.One * blastSpeed * delta);
+	}	
 }
