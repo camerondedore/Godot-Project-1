a62a8d8fc59bd9e1d9044af77920c29be57820b5
camerondedore <camerondedore@gmail.com>
2022-09-17 22:19:35 -0700

    adding inertia to casing ejector

--- C#/Weapons/CasingEjector.cs
+++ C#/Weapons/CasingEjector.cs
@@ -10,15 +10,27 @@ public class CasingEjector : Spatial
     Vector3 direction;
     [Export]
     float spread = 1;
+    Vector3 lastPosition,
+        velocityDelta,
+        velocity;
     
 
 
     
     public override void _Ready()
     {
-        
+        lastPosition = GlobalTransform.origin;
     }
 
+
+
+    public override void _PhysicsProcess(float delta)
+    {
+        // get velocity for inertia
+        velocityDelta = GlobalTransform.origin - lastPosition;
+        velocity = velocityDelta / delta;
+        lastPosition = GlobalTransform.origin;
+    }
 
 
 
@@ -37,7 +49,7 @@ public class CasingEjector : Spatial
         casing.LookAtFromPosition(GlobalTransform.origin, GlobalTransform.origin + GlobalTransform.basis.z * -100, Vector3.Up);
 
         // eject rigid body
-        casing.LinearVelocity = ejectionDirection;
+        casing.LinearVelocity = ejectionDirection + velocity;
         casing.AngularVelocity = new Vector3(GD.Randf() - 0.5f, GD.Randf() - 0.5f, GD.Randf() - 0.5f) * 3.14f * 15;
 
         // set parent as root scene
