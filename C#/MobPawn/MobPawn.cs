c636e19e9198580baf3e7e2c75901e6627a2617e
camerondedore <camerondedore@gmail.com>
2022-09-23 21:56:54 -0700

    starting pawn

---
+++ C#/MobPawn/MobPawn.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public class MobPawn : KinematicBody
+{
+    
+    public StateMachine machine = new StateMachine();
+    //public State stateStart;
+
+
+
+    public override void _Ready()
+    {
+        // get nodes
+		//cameraSpringArm = GetNode<CameraSpringArm>("CameraSpringArm");
+        
+        // initialize states
+		//stateIdle = new CharacterStateIdle(){blackboard = this};
+
+		// set first state in machine
+		//machine.SetState(stateStart);
+    }
+
+
+
+    public override void _Process(float delta)
+    {
+        
+    }
+}
